5e2669bfc61d9ee8c6d46aeee93abcd9d3bc54e1
emiiryv <111075935+emiiryv@users.noreply.github.com>
2023-12-23 20:01:41 +0300

    Initial commit

---
+++ src/Kedi.java
@@ -0,0 +1,10 @@
+class Kedi extends Hayvan{
+    public Kedi(String isim) {
+        super(isim);
+    }
+
+    @Override
+    public String konus() {
+        return this.getIsim() + " Miyavlıyor...";
+    }
+}
---
+++ src/Kopek.java
@@ -0,0 +1,10 @@
+class Kopek extends Hayvan {
+    public Kopek(String isim) {
+        super(isim);
+    }
+
+    @Override
+    public String konus() {
+        return this.getIsim() + " Havlıyor...";
+    }
+}
